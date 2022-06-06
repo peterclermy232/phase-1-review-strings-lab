e9511aacb136c3352b738e907aa91173c836c0bc
peterclermy <peterclermy232@gmail.com>
2022-06-06 11:10:51 +0300

    string

---
+++ .history/index_20220606104854.js
@@ -0,0 +1,4 @@
+// Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+const welcome
---
+++ .history/index_20220606105903.js
@@ -0,0 +1,10 @@
+// Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+const WelcomeMessage = 'Welcome to Flatbook, ';
+
+const WelcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.splice(0,1)}!`;
